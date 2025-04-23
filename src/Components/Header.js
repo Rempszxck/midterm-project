4379cf05b71037904775ba67a5d4a8f394bdedd4
Rempszxck <mankeygaming069@gmail.com>
2025-04-23 13:12:26 +0800

    Add files via upload

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <div>
+        <label htmlFor="search" className="title-label">
+        Visualizing Country Information
+        
+        </label>
+      </div>
+     
+  </header>
+  );
+}
+export default Header;
